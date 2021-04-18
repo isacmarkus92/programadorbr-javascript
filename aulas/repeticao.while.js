d98d2d23565def8282c2c94eb8f4c1c93371ebeb
Isac Markus <isacmarkus92@gmail.com>
2021-04-18 13:36:12 -0300

    organizacao da arvore

--- aulas/repeticao.while.js
+++ aulas/repeticao.while.js
@@ -1,12 +1,3 @@
-var numero = 5;
-
-//while = enquanto
-while (numero < 10) {
-    console.log("Número " + numero);
-    numero++;
-}
-console.log("Acabou")
-
 //primeiro exemplo
 
 var numero = Math.random() * 100;
@@ -16,4 +7,13 @@ while (numero < 90) {
     numero = Math.random() * 100;
 }
 console.log(numero + " Neste ponto extrapolou");
+
+//segundo exemplo
+var numero = 5;
+//palavra while = enquanto
+while (numero < 10) {
+    console.log("Número " + numero);
+    numero++;
+}
+console.log("Acabou")
 console.log("Acabou");
