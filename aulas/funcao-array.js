5483707d9105b6a7d3cb9abca153c203681b498a
Isac Markus <isacmarkus92@gmail.com>
2021-04-18 12:40:32 -0300

    funcao-array-aplicada
    Funcionalidades do array. Bloco de informações em linha que contém variáveis para aplicação como for in que ajudam na extração de dados específicos.

---
+++ aulas/funcao-array.js
@@ -0,0 +1,34 @@
+// var alunos = new Array("Igor", "Jose", "Marcos", "Mariana")
+
+// console.log(alunos[0])
+//primeira forma de fazer um Array
+
+// var alunos = ["Igor", "Jose", "Marcos", "Mariana"]
+
+// console.log(alunos.length)
+// console.log(alunos[0])
+// segunda forma de fazer Array
+
+var alunos = ["Igor", "José", "Marcos", "Mariana",
+    "Joana", "Leo", "João"
+]
+
+for (var i = 0; i < alunos.length; i++) {
+    console.log(alunos[i])
+}
+//usando in
+var alunos = ["Igor", "José", "Marcos", "Mariana",
+    "Joana", "Leo", "João"
+]
+
+for (var i in alunos) {
+    console.log(i)
+}
+//usando of
+var alunos = ["Igor", "José", "Marcos", "Mariana",
+    "Joana", "Leo", "João"
+]
+
+for (var aluno of alunos) {
+    console.log(aluno)
+}
---
+++ aulas/tiposDeDados.js
@@ -0,0 +1,109 @@
+// tipos de dados
+
+// Boolean
+// Null
+// Undefined
+// Number
+// String - uma cadeia de caracteres
+var a = "Palavra\nMorta"
+
+console.log(a)
+
+// Null é a certeza de que a variável não tem nenhuma infomação ou ocupando um espaço de memória
+var b = null
+
+console.log(b)
+
+//Boolean
+
+// + Adição
+// - Subtração
+
+var c = 10;
+var d = 15;
+
+var e = c + d;
+console.log(e);
+
+// * Multiplicação
+
+var f = 10;
+var g = 15;
+
+console.log(f * g);
+
+// / Divisão
+
+var h = 10;
+var i = 15;
+
+var j = h / i;
+console.log(j);
+
+// % módulo
+
+var l = 10;
+var m = 15;
+
+var n = l % m;
+console.log(n);
+
+// ++ INcremento
+
+var o = 10;
+
+o++;
+
+console.log(o);
+
+// -- Decremento
+
+var p = 10;
+
+p--;
+
+console.log(p);
+
+// =    x = y       x = y
+// +=   x += y      x = x + y
+// -=   x -= y      x = x - y
+// *=   x *= y      x = x * y
+// /=   x /= y      x = x / y
+// %=   x %= y      x = x % y
+
+var q = 30;
+
+// as variáveis abaixo dão o mesmo resultado = variável + 1
+
+q++;
+
+q = q + 1;
+
+q += 1;
+
+console.log(q);
+
+// == igual a
+// === valor igual e igual
+// ! = não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor que
+// > = maior que ou igual a
+// <= menor ou igual a
+
+console.log(2 == 3);
+
+console.log(2 === 3);
+
+console.log(2 != 3);
+
+console.log(2 !== 3);
+
+console.log(2 > 3);
+
+console.log(2 < 3);
+
+console.log(2 >= 3);
+
+console.log(2 <= 3);
