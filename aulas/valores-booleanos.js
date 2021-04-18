7a0cef0323ff28d09deeb125c5223636051a697f
Isac Markus <isacmarkus92@gmail.com>
2021-04-18 14:53:44 -0300

    valores booleanos
    Os valores booleanos retornam sempre uma condição false ou true. Eles nos ajudam a categorizar as informações e aceitá-las ou não como pré condições de carregamentos posteriores. Isso ajuda a identificar que uma condição é aceitável e só então gerar uma nova demanda funcional ou não.

---
+++ aulas/valores-booleanos.js
@@ -0,0 +1,26 @@
+var a = 3;
+
+var b = "3"
+
+var a = true;
+var b = false;
+var c = a && b
+
+console.log(c);
+
+var idade = 9;
+var menor10 = idade < 10;
+var maior65 = idade > 65;
+
+var gratuidade = menor10 || maior65;
+
+console.log("Idade", idade)
+console.log("Maior que 65", maior65)
+console.log("Menor que 10", menor10)
+console.log("Tem direito a gratuidade", gratuidade)
+
+//Negativo
+
+// console.log(!false)
+
+// console.log(!true)
