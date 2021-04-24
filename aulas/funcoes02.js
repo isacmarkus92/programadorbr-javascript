1120e009cb722ca45c2560b1119e0bdb7db14cfa
Isac Markus <isacmarkus92@gmail.com>
2021-04-24 00:45:20 -0300

    funções-anonimas
    A caracteristica principal dela é que sua declaração é feita dentro de uma informação já existente como uma variável por exemplo.

---
+++ aulas/funcoes02.js
@@ -0,0 +1,6 @@
+var media = function(n1, n2) {
+    return (n1 + n2) / 2;
+}
+console.log(media(5, 6));
+
+//forma de simplificação da função utilizando ela como função anonima, usando uma função dentro de uma variável
