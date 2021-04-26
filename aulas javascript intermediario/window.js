7d3b4c14b72422ef737910dfdab21d2298d7a8e3
Isac Markus <isacmarkus92@gmail.com>
2021-04-25 22:36:03 -0300

    Window e suas propriedades

---
+++ aulas javascript intermediario/window.js
@@ -0,0 +1,9 @@
+//window é um ojeto global com várias atribuições ou propriedades
+
+//abra o navegador para entender essa aula
+
+window.console.log("Teste");
+
+console.log("Outro teste");
+
+alert("Essa página acabou de ser iniciada")
